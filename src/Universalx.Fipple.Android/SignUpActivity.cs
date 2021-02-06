b64a2637b70f00ecc1a009ead14c8f2c0c620a3f
Arman Sahakyan <arman.sahakyan.dev@outlook.com>
2021-02-06 23:02:45 +0400

    Added SignUp activity, Improved MainActivity

--- src/Universalx.Fipple.Android/SignUpActivity.cs
+++ src/Universalx.Fipple.Android/SignUpActivity.cs
@@ -2,34 +2,24 @@
 using Android.OS;
 using Android.Runtime;
 using Android.Support.V7.App;
-using Android.Widget;
-using System;
 
 namespace Universalx.Fipple.Android
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+    public class SignUpActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
-            
-            Button btnSignUp = FindViewById<Button>(Resource.Id.btnSignUp);
-            btnSignUp.Click += GoToSignUpActivity;
+            SetContentView(Resource.Layout.activity_signUp);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] global::Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-
-        public void GoToSignUpActivity(object sender, EventArgs e)
-        {
-            StartActivity(typeof(SignUpActivity));
         }
     }
 }
